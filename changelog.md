f81b44ca4e1d561c361ce7ec8d96d8e4a9e7e50b
Neo Yuki Aylor <neoyukiaylor@gmail.com>
2025-02-14 15:40:49 +0000

    GD v2.2074 | Geode v2.4.0

--- changelog.md
+++ changelog.md
@@ -3,3 +3,8 @@
 
 # v1.1.0
  * Updated to GD v2.206 and Geode V3
+
+# v1.2.0
+ * Updated to GD v2.2074 and Geode V4
+ * Instead of hijacking the copy level id button, the mod now creates a new button entirely
+ * Removed the song ID copy code as the introduction of songs that arent on newgrounds makes any level which uses nongs point to invalid links
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/InfoLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(InfoLayer) {
-	void onCopyLevelID(CCObject*) {
-		/*
-			Get the name of the author and the level name then string them together
-			seperating them with " - " and append it with "(Geometry Dash)" lastly,
-			store the result in a variable called "ytVideoTitle" for later use
-		*/
-		std::string ytVideoTitle = fmt::format("{} - {} (Geometry Dash)", this->m_level->m_creatorName, this->m_level->m_levelName);
-		
-		/* 
-			Get the length of the "ytVideoTitle" variable and use the returned value to create
-			a string of "=" symbols which is the same length as the "ytVideoTitle" variable
-		*/
-		std::string ytVideoDivider = std::string(ytVideoTitle.length(), '=');
-		
-		// Get the ID of the level and store it in a variabled called "ytVideoDescLevelID" for later use
-		std::string ytVideoDescLevelID = std::to_string(this->m_level->m_levelID.value());
-		
-		// Prepare and copy the formatted data above to the clipboard for easy pasting wherever the user wants
-		clipboard::write(fmt::format("{}\n{}\nLevel ID: {}\n", ytVideoTitle, ytVideoDivider, ytVideoDescLevelID));
+class $modify(NeoLayer, LevelInfoLayer) {
+  void onCopyInfo(CCObject * target) {
+    auto YTvTitle = fmt::format("{} - {} (Geometry Dash)", this->m_level->m_creatorName, this->m_level->m_levelName);
+    auto YTvDesc = std::to_string(this->m_level->m_levelID.value());
+    auto test = clipboard::write(fmt::format("{}\nLevel ID: {}\n", YTvTitle, YTvDesc));
+    Notification::create(("Level info copied to clipboard!"), CCSprite::create("Copy.png"_spr), 3)->show();
+  }
 
-		// Notify the player to let them know that all the level information has been copied to their clipboard
-		Notification::create(("Level info copied to clipboard!"), CCSprite::create("Copy.png"_spr), 3)->show();
-	}
+  bool init(GJGameLevel * var1, bool var2) {
+    if (!LevelInfoLayer::init(var1, var2)) return false;
+
+    auto button = CCMenuItemSpriteExtra::create(
+      CircleButtonSprite::createWithSpriteFrameName("Copy.png"_spr,
+        .8f,
+        CircleBaseColor::Green,
+        CircleBaseSize::Medium
+      ), this, menu_selector(NeoLayer::onCopyInfo));
+    button->setID("neo-copy-info-button");
+
+      if (auto menu = getChildByID("left-side-menu")) {
+        menu->addChild(button);
+        menu->updateLayout();
+      }
+
+      return true;
+  }
 };
